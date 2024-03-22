b64e91216b7f6bc290d2d66e7e9a38f64d69f5ab
lisapeters13 <lisapetersirl@gmail.com>
2024-03-22 16:51:56 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function getCustomerName() {
+    return customerName;
+  }
+
+  console.log(getCustomerName());
+
+
+  function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  
+  var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
+  
+  const leastFavoriteCustomer = "john";
+  function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = "Alice";
+  }
+  console.log(leastFavoriteCustomer); // Output: "John"
+  changeLeastFavoriteCustomer(); // This will throw an error
+  console.log(leastFavoriteCustomer); // Output: "John"
+
+  
